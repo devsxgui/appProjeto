5e3a94d30dd7870a4410ae23842d9335e510639f
devsxgui <104882033+devsxgui@users.noreply.github.com>
2022-11-30 21:51:39 -0300

    Update connectionFirebase.js

--- src/services/connectionFirebase.js
+++ src/services/connectionFirebase.js
@@ -9,12 +9,7 @@ import 'firebase/compat/database';
 
 // Firebase app configuration
 let firebaseConfig = {
-  apiKey: "AIzaSyA68rs14ZBFY1xRyr8IaA92vqAPXiZT3a0",
-  authDomain: "bancoloja-66431.firebaseapp.com",
-  projectId: "bancoloja-66431",
-  storageBucket: "bancoloja-66431.appspot.com",
-  messagingSenderId: "752494005158",
-  appId: "1:752494005158:web:f7385caeaf26718b5d9d67"
+
 };
 
 //Open and close connection
